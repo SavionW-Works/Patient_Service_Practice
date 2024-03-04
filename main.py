3be4a79c8530c96e3c4493e63bb8e0fb901c2d42
SavionW-Works <72318904+SavionW-Works@users.noreply.github.com>
2024-03-04 18:13:00 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Beginning of app.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# "uvicorn main:app --reload" may not work
+# Use: "python -m uvicorn main:app --reload" instead 
+# Make sure you're in the PATIENT_SERVICE_API folder, and not any deeper
+
+# Create a virtual environment with "python -m venv env" 
+
+# After starting the virtual environment, activate it in bash with: 
+'''
+$ cd env/Scripts/
+$ . activate  OR $ .[backslash]activate
+'''
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"} 
+
+@app.get("/users")
+async def getUsers():
+    return [
+    {
+        "firstName": "Some", 
+        "lastName": "One", 
+        "email": "someone@gmail.com"
+        
+    } 
+    ]
